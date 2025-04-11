58e97bace79e22d63a68101b461c5a1a00ba40d6
minsuk <puhi0823@snu.ac.kr>
2025-04-11 11:34:09 +0900

    2022-17733

--- calculator.cpp
+++ calculator.cpp
@@ -2,17 +2,17 @@
 #include <stdexcept>
 
 int Calculator::add(int a, int b) {
-    // TODO
+    return a + b;
 }
 
 int Calculator::sub(int a, int b) {
-    // TODO
+    return a - b;
 }
 
 int Calculator::mul(int a, int b) {
-    // TODO
+    return a * b;
 }
 
 int Calculator::div(int a, int b) {
-    // TODO
+    return a / b;
 }

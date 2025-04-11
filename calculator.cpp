99938cd194b1f41b3cfbca83f2225f7bc866d44f
minsuk <puhi0823@snu.ac.kr>
2025-04-11 11:55:44 +0900

    insert new int

--- calculator.cpp
+++ calculator.cpp
@@ -2,17 +2,25 @@
 #include <stdexcept>
 
 int Calculator::add(int a, int b) {
-    return a + b;
+    int c;
+    c = a + b;
+    return c;
 }
 
 int Calculator::sub(int a, int b) {
-    return a - b;
+    int c;
+    c = a - b;
+    return c;
 }
 
 int Calculator::mul(int a, int b) {
-    return a * b;
+    int c;
+    c = a * b;
+    return c;
 }
 
 int Calculator::div(int a, int b) {
-    return a / b;
+    int c;
+    c = a / b;
+    return c;
 }
